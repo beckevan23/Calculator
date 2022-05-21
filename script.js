355f080edbf7b77d36b5971d147e26b33676a298
nefarox <evan.beck23@gmail.com>
2022-05-21 10:43:08 -0500

    Start logic for calculations

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function add(x, y) {return x + y};
+function add(x, y) {return +x + +y};
 function subtract(x, y) {return x - y};
 function multiply(x, y) {return x * y};
 function divide(x, y) {return x / y};
@@ -6,14 +6,49 @@ function divide(x, y) {return x / y};
 function operate(operator, x, y) {
     switch(operator) {
         case "+":
-            return add(x, y);
+            array[0] = add(x, y);
+            array.splice(1, 2)
+            display.textContent = array.join(" ");
+            break;
         case "-":
-            return subtract(x, y);
+            array[0] = subtract(x, y);
+            array.splice(1, 2)
+            display.textContent = array.join(" ");
+            break;
         case "x":
-            return multiply(x, y);
+            array[0] = multiply(x, y);
+            array.splice(1, 2)
+            display.textContent = array.join(" ");
+            break;
         case "/":
-            return divide(x, y);
+            array[0] = divide(x, y);
+            array.splice(1, 2)
+            display.textContent = array.join(" ");
+            break;
         default:
             return "Error"
     };
 };
+let display = document.querySelector(".display");
+let buttons = Array.from(document.querySelectorAll('button'));
+let array;
+buttons.forEach(button => button.addEventListener("click", function(){
+    if (button.textContent == "0" || button.textContent == "1" ||
+        button.textContent == "2" || button.textContent == "3" ||
+        button.textContent == "4" || button.textContent == "5" ||
+        button.textContent == "6" || button.textContent == "7" ||
+        button.textContent == "8" || button.textContent == "9") {
+            display.textContent += button.textContent;
+    } else if (button.textContent == "x" || button.textContent == "-" ||
+               button.textContent == "+" || button.textContent == "/") {
+        display.textContent += " " + button.textContent + " ";
+    } else if (button.textContent == "=") {
+        array = display.textContent.split(" ");
+        console.log(array)
+        operate(array[1], array[0], array[2]);
+    } else if (button.textContent == "c") {
+        display.textContent = "";
+    }
+}))
+
+
